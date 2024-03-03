ba7175f3efc6e6a38a8b4108e6f2d99a5ab41c1a
shivang51 <aad9805066254@gmail.com>
2024-03-03 12:53:03 +0530

    cursor moving on tab press

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -24,3 +24,16 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.treesitter.start(0, "markdown")
 	end,
 })
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "*",
+	callback = function()
+		if
+			((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+			and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+			and not require("luasnip").session.jump_active
+		then
+			require("luasnip").unlink_current()
+		end
+	end,
+})
