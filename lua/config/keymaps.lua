9e4ef26d4400c955d208d5f4f22ffd036f10f6e9
Shivang Sharma <aad9805066254@gmail.com>
2025-03-02 14:10:28 +0530

    edited blink completions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,29 +3,29 @@ vim.keymap.set({ "i", "n" }, "<C-s>", "<Cmd>w<CR>")
 vim.keymap.set("t", "<Esc><Esc>", "<c-\\><c-n>")
 
 vim.keymap.set("n", "<C-M-t>", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 8)
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 8)
 end, { desc = "Open Terminal" })
 
 -- lsp keymaps
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
-vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "[G]oto [D]efinition" })
 vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "[G]oto [R]eferences" })
 vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "[G]oto [I]mplementation" })
 vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "Type [D]efinition" })
 vim.keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
 vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols,
-  { desc = "[W]orkspace [S]ymbols" })
+	{ desc = "[W]orkspace [S]ymbols" })
 
 
 vim.keymap.set("n", "<leader>/", function()
-  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-    winblend = 10,
-    previewer = false,
-  }))
+	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		winblend = 10,
+		previewer = false,
+	}))
 end, { desc = "[/] Fuzzily search in current buffer]" })
 
 vim.keymap.set("n", "<space>sf", require('telescope.builtin').find_files, { desc = "[S]earch [F]iles" })
@@ -38,3 +38,10 @@ vim.keymap.set("n", "dp", vim.diagnostic.goto_prev, { desc = "Go to [D]iagnostic
 vim.keymap.set("n", "dn", vim.diagnostic.goto_next, { desc = "Go to [D]iagnostics [N]ext" })
 vim.keymap.set("n", "df", vim.diagnostic.open_float, { desc = "Open [D]iagnostics [F]loating window" })
 vim.keymap.set("n", "dq", vim.diagnostic.setloclist, { desc = "Set diagnostic loc list" })
+
+-- switch between two buffers
+vim.keymap.set("n", "<c-tab>", "<c-^>", { desc = "Swith between two buffers" })
+
+-- insert lines and escape
+vim.keymap.set({ "n" }, "o", "o<Esc>", { desc = "Create new line below" })
+vim.keymap.set({ "n" }, "O", "O<Esc>", { desc = "Create new line above" })
--- lua/config/plugins/blink-completions.lua
+++ lua/config/plugins/blink-completions.lua
@@ -1,50 +1,34 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = 'rafamadriz/friendly-snippets',
-
-  version = '*',
-  opts = {
-    keymap = {
-      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-      ['<C-e>'] = { 'hide', 'fallback' },
-      ['<CR>'] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
-        end,
-        'snippet_forward',
-        'fallback'
-      },
-      ['<Tab>'] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
-        end,
-        'snippet_forward',
-        'fallback'
-      },
-      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-      ['<Up>'] = { 'select_prev', 'fallback' },
-      ['<Down>'] = { 'select_next', 'fallback' },
-      ['<C-p>'] = { 'select_prev', 'fallback' },
-      ['<C-n>'] = { 'select_next', 'fallback' },
-      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-    },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
-    },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    signature = { enabled = true }
-  },
-  opts_extend = { "sources.default" }
+	'saghen/blink.cmp',
+	dependencies = 'rafamadriz/friendly-snippets',
+	version = '*',
+	opts = {
+		keymap = {
+			['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+			['<C-e>'] = { 'hide', 'fallback' },
+			['<Tab>'] = {
+				function(cmp)
+					if not cmp.snippet_active() then
+						return cmp.select_and_accept()
+					end
+				end,
+				'snippet_forward',
+				'fallback'
+			},
+			['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+			['<Up>'] = { 'select_prev', 'fallback' },
+			['<Down>'] = { 'select_next', 'fallback' },
+			['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+			['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+		},
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = 'mono'
+		},
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
+		},
+		signature = { enabled = true }
+	},
+	opts_extend = { "sources.default" }
 }
